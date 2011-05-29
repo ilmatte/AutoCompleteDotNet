ce59eedc437162eaf7848ed34a03d9307dab4a0a
Matteo Tontini <3607516+ilmatte@users.noreply.github.com>
2011-05-29 09:47:12 +0000

    Changed AutoCompleteSettingsDto's properties visibility from public to internal

--- AutoComplete/Common/AutoCompleteSettingsDto.cs
+++ AutoComplete/Common/AutoCompleteSettingsDto.cs
@@ -16,19 +16,19 @@
         /// Represents the id to retrieve the input control
         /// to provide with autocomplete functionalities.
         /// </summary>
-        public string ControlId { get; set; }
+        internal string ControlId { get; set; }
 
         /// <summary>
         /// Minimum number of chars to digit before the autocomplete is fired.
         /// </summary>
-        public int MinChars { get; set; }
+        internal int MinChars { get; set; }
 
         /// <summary>
         /// An Uri identifying a resource returned in Json format.
         /// Not relying on automatic property allows us to resolve
         /// the ~ character in Urls.
         /// </summary>
-        public Uri Url { get; set; }
+        internal Uri Url { get; set; }
 
         /// <summary>
         /// Specifies the method to be used for the http invocation:
@@ -36,7 +36,7 @@
         /// If a value is not specified for this property it will
         /// default to GET.
         /// </summary>
-        public HttpMethod HttpMethod
+        internal HttpMethod HttpMethod
         {
             get
             {
@@ -51,13 +51,13 @@
         /// <summary>
         /// A javascript function to be invoked when a suggested item is selected.
         /// </summary>
-        public string SelectionCallback { get; set; }
+        internal string SelectionCallback { get; set; }
 
         /// <summary>
         /// A javascript function to be invoked when an error occurs during the
         /// ajax request to retrieve suggestions.
         /// </summary>
-        public string ErrorCallback { get; set; }
+        internal string ErrorCallback { get; set; }
 
         /// <summary>
         /// Specifies the name of a property of the returned object type.
@@ -66,7 +66,7 @@
         /// If it is not specified the value in the property: 
         /// ValueField will be used in the autocomplete initialization.
         /// </summary>
-        public string LabelField { get; set; }
+        internal string LabelField { get; set; }
 
         /// <summary>
         /// Specifies the name of a property of the returned object type.
@@ -75,7 +75,7 @@
         /// If a LabelField property is not specified this property's value
         /// will be used as LabelField value in the autocomplete initialization.
         /// </summary>
-        public string ValueField { get; set; }
+        internal string ValueField { get; set; }
 
         /// <summary>
         /// Specifies if a reference to the jquery and jquery UI javascript
@@ -84,7 +84,7 @@
         /// jquery 1.4.4 and jquery.ui 1.8.7
         /// Default value is false.
         /// </summary>
-        public bool RegisterJQueryAndJQueryUI { get; set; }
+        internal bool RegisterJQueryAndJQueryUI { get; set; }
 
         /// <summary>
         /// Specifies if a reference to a default autocomplete stylesheet 
@@ -93,7 +93,7 @@
         /// this property is not necessary.
         /// Default value is false.
         /// </summary>
-        public bool RegisterDefaultStyleSheet { get; set; }
+        internal bool RegisterDefaultStyleSheet { get; set; }
 
         /// <summary>
         /// Specifies the text to be displaied in the autosuggest list when
@@ -101,7 +101,7 @@
         /// Such property will be used to display an informational message
         /// in the dropdownlist.
         /// </summary>
-        public string ErrorMessage { get; set; }
+        internal string ErrorMessage { get; set; }
 
         /// <summary>
         /// Specifies the text to be displaied in the autosuggest list when
@@ -109,7 +109,7 @@
         /// Such property will be used to display an informational message
         /// in the dropdownlist.
         /// </summary>
-        public string NoResultsMessage { get; set; }
+        internal string NoResultsMessage { get; set; }
 
         #endregion
     }
